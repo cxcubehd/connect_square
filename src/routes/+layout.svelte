3814ad96b5ad1206e9b5fe0b46ea8d6146e5f00b
CXCubeHD <cxcubehd@gmail.com>
2026-02-07 00:09:53 +0100

    fix: replace favicon.svg with favicon.ico and add logo.jpg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { ModeWatcher } from 'mode-watcher';
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 
 	let { children } = $props();
 </script>
